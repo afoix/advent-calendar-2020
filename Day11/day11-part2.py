ab565560e99a224741ed79446c9a0dcb63f2164c
Richard Fine <rfine@tbrf.net>
2020-12-23 11:24:50 -0500

    Make day 11 solution more readable

--- Day11/day11-part2.py
+++ Day11/day11-part2.py
@@ -1,36 +1,80 @@
-with open('Day11/input.txt') as input:
+with open('Day11/seatsOccupiedPattern.txt') as input:
     seats = [[char for char in line.strip()] for line in input]
 
-while True:
+(maxX, maxY) = (len(seats[0]), len(seats))
+
+def countOccupiedSeatsVisibleFrom(seats, x, y):
+    
+    occupied = 0
+
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            # Do not count the seat in the middle
+            if dx == 0 and dy == 0: continue
+
+            # Trace a ray along (dx, dy), testing the seat at each position
+            distance = 1
+            while True:
+                # Calculate the position we are looking at this time around
+                (ix, iy) = (x + distance * dx, y + distance * dy)
+
+                # Stop tracing if we hit the edge of the area
+                if ix < 0 or iy < 0: break
+                if ix >= maxX or iy >= maxY: break
+
+                # If this is floor, keep tracing past it
+                if seats[iy][ix] == '.':
+                    distance += 1
+                    continue
+
+                # It's a seat - if it is occupied, count it, then stop tracing
+                if seats[iy][ix] == '#': 
+                    occupied += 1
+                break
+
+    return occupied
+
+def stepSimulation(seats):
+    # Make a copy of the previous state
     step = [line.copy() for line in seats]
+
+    # Keep track of whether anything actually changed in this step
     any_changes = False
-    for y in range(len(step)):
-        row = step[y]
-        for x in range(len(row)):
-            if row[x] == '.': continue
-            adjacent = 0
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
-                    if dx == 0 and dy == 0: continue
-                    i = 1
-                    while True:
-                        if y + i*dy < 0 or x + i*dx < 0: break
-                        if y + i*dy >= len(step) or x + i*dx >= len(row): break
-                        if seats[y+i*dy][x+i*dx] == '.':
-                            i += 1
-                            continue
-                        if seats[y+i*dy][x+i*dx] == '#': adjacent += 1
-                        break
-            if row[x] == '#':
-                if adjacent >= 5:
-                    row[x] = 'L'
-                    any_changes = True
-            elif row[x] == 'L':
-                if adjacent == 0:
-                    row[x] = '#'
-                    any_changes = True
-    if not any_changes: break
+
+    # Loop over all the positions
+    for y in range(maxY):
+        for x in range(maxX):
+            # Skip floor
+            if seats[y][x] == '.': continue
+
+            occupied = countOccupiedSeatsVisibleFrom(seats, x, y)
+
+            # If the seat is occupied, and has more than 5 occupied adjacent seats,
+            # it becomes empty
+            if seats[y][x] == '#' and occupied >= 5:
+                step[y][x] = 'L'
+                any_changes = True
+
+            # If the seat is empty, and has no occupied adjacent seats, 
+            # it becomes occupied
+            elif seats[y][x] == 'L' and occupied == 0:
+                step[y][x] = '#'
+                any_changes = True
+
+    # If nothing changed, indicate that by returning None
+    if not any_changes:
+        return None
+
+    # Otherwise, return the new state
+    return step
+
+# Main loop: Keep stepping the simulation until it no longer changes (returns None)
+while True:
+    step = stepSimulation(seats)
+    if step is None: break
     seats = step
 
+# Count the total number of occupied seats
 total = sum([sum([1 for char in row if char == '#']) for row in seats])
+
 print (total)
--- Day11/day11.py
+++ Day11/day11.py
@@ -1,30 +1,68 @@
-with open('Day11/input.txt') as input:
+with open('Day11/seatsOccupiedPattern.txt') as input:
     seats = [[char for char in line.strip()] for line in input]
 
-while True:
+(maxX, maxY) = (len(seats[0]), len(seats))
+
+def countOccupiedSeatsAdjacentTo(seats, x, y):
+    
+    adjacent = 0
+
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            # Do not count the seat in the middle
+            if dx == 0 and dy == 0: continue
+
+            # Do not try to count positions that are outside the edges of the seating area
+            if y + dy < 0 or x + dx < 0: continue
+            if y + dy >= maxY or x + dx >= maxX: continue
+
+            # If the seat is occupied, increment the counter
+            if seats[y+dy][x+dx] == '#': 
+                adjacent += 1
+
+    return adjacent
+
+def stepSimulation(seats):
+    # Make a copy of the previous state
     step = [line.copy() for line in seats]
+
+    # Keep track of whether anything actually changed in this step
     any_changes = False
-    for y in range(len(step)):
-        row = step[y]
-        for x in range(len(row)):
-            if row[x] == '.': continue
-            adjacent = 0
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
-                    if dx == 0 and dy == 0: continue
-                    if y + dy < 0 or x + dx < 0: continue
-                    if y + dy >= len(step) or x + dx >= len(row): continue
-                    if seats[y+dy][x+dx] == '#': adjacent += 1
-            if row[x] == '#':
-                if adjacent >= 4:
-                    row[x] = 'L'
-                    any_changes = True
-            elif row[x] == 'L':
-                if adjacent == 0:
-                    row[x] = '#'
-                    any_changes = True
-    if not any_changes: break
+
+    # Loop over all the positions
+    for y in range(maxY):
+        for x in range(maxX):
+            # Skip floor
+            if seats[y][x] == '.': continue
+
+            adjacent = countOccupiedSeatsAdjacentTo(seats, x, y)
+
+            # If the seat is occupied, and has more than 4 occupied adjacent seats,
+            # it becomes empty
+            if seats[y][x] == '#' and adjacent >= 4:
+                step[y][x] = 'L'
+                any_changes = True
+
+            # If the seat is empty, and has no occupied adjacent seats, 
+            # it becomes occupied
+            elif seats[y][x] == 'L' and adjacent == 0:
+                step[y][x] = '#'
+                any_changes = True
+
+    # If nothing changed, indicate that by returning None
+    if not any_changes:
+        return None
+
+    # Otherwise, return the new state
+    return step
+
+# Main loop: Keep stepping the simulation until it no longer changes (returns None)
+while True:
+    step = stepSimulation(seats)
+    if step is None: break
     seats = step
 
+# Count the total number of occupied seats
 total = sum([sum([1 for char in row if char == '#']) for row in seats])
+
 print (total)
