343cc4ea65876a20f544837b8b33941bf0a65832
Richard Fine <richardf@unity3d.com>
2021-01-01 21:54:27 -0500

    Comments for day 15

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,33 +1,51 @@
+# Starting numbers
 input = [1,0,15,2,10,13]
+
+# lastSpokenAt is a map from each number to 'what the turn number was the last time this number was spoken'
 lastSpokenAt = {}
+
+# lastLastSpokenAt is a map from each number to 'what the turn number was the time before the last time this number was spoken'
 lastLastSpokenAt = {}
 
-for i in range(len(input)):
-    lastSpokenAt[input[i]] = i + 1 # turns are 1-based, not 0-based
+# Start on turn 1
+turn = 1
 
-lastSpoken = input[-1]
-turn = len(input) + 1
+def speak(number):
+    global turn, lastSpokenAt, lastLastSpokenAt, lastSpoken
+    # If this number has been spoken before, move its entry from lastSpokenAt into lastLastSpokenAt
+    if number in lastSpokenAt:
+        lastLastSpokenAt[number] = lastSpokenAt[number]
+    
+    # Store this number into lastSpokenAt
+    lastSpokenAt[number] = turn
 
-while turn <= 2020:
-    if lastSpoken not in lastLastSpokenAt:
-        lastSpoken = 0
-    else:
-        lastSpoken = lastSpokenAt[lastSpoken] - lastLastSpokenAt[lastSpoken]
-    if lastSpoken in lastSpokenAt:
-        lastLastSpokenAt[lastSpoken] = lastSpokenAt[lastSpoken]
-    lastSpokenAt[lastSpoken] = turn
+    lastSpoken = number
     turn += 1
+
+def playOneTurn():
+    # "If that was the first time the number has been spoken..."
+    # (i.e. the number is has no entry in the 'the time before last time' map):
+    if lastSpoken not in lastLastSpokenAt:
+        # "...the current player says 0."
+        speak(0)
+
+    # "Otherwise, the number had been spoken before; ..."
+    else:
+        # "...the current player announces how many turns apart the number is from when it was previously spoken."
+        speak(lastSpokenAt[lastSpoken] - lastLastSpokenAt[lastSpoken])
+
+# Initialize with the starting numbers
+for number in input:
+    speak(number)
+
+# Part 1: what will be the 2020th number spoken?
+while turn <= 2020:
+    playOneTurn()
 
 print(lastSpoken)
 
+# Part 2: what will be the 30000000th number spoken?
 while turn <= 30000000:
-    if lastSpoken not in lastLastSpokenAt:
-        lastSpoken = 0
-    else:
-        lastSpoken = lastSpokenAt[lastSpoken] - lastLastSpokenAt[lastSpoken]
-    if lastSpoken in lastSpokenAt:
-        lastLastSpokenAt[lastSpoken] = lastSpokenAt[lastSpoken]
-    lastSpokenAt[lastSpoken] = turn
-    turn += 1
+    playOneTurn()
 
 print(lastSpoken)
